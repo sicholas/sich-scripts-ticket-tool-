04b289e7fde3bce778da6138d9a823ab35975cdd
sicholas <56162864+sicholas@users.noreply.github.com>
2020-07-12 08:18:09 -0400

    Update bot.py
    Archive Update

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import datetime
 import os
 from discord.utils import get
 import discord
-from discord import Embed
+from discord import Embed, CategoryChannel
 from discord.ext.commands import bot
 
 from TicketState import Ticket, TicketState
@@ -26,7 +26,18 @@ async def on_ready():
 @client.event
 async def on_message(message):
     print(message.channel.id)
-    if message.channel.id == 729922205106045069:
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(message.author.roles)
+    role_names = [role.name for role in message.author.roles]
+    if message.content == "!archive" and "Scripter" in role_names:
+        print("Archiving")
+        channel = message.channel
+        category = discord.utils.get(guild.categories, id=731843594708713553)
+        await channel.edit(category=category)
+    elif message.channel.id == 729922205106045069:
         ticket_created = False
         for guild in client.guilds:
             if guild.name == GUILD:
@@ -35,12 +46,10 @@ async def on_message(message):
             if channel.name == message.author.display_name:
                 ticket_created = True
                 break
-        if message.content.startswith('!request') and message.content != '!request' and ticket_created == False:
-
-
+        if ticket_created == False:
             display = message.author.display_name
             author = message.author
-            theMessage = str(message.content).split('!request ')[1]
+            theMessage = message.content
             guild = message.guild
             await message.delete()
             overwrites = {
@@ -57,10 +66,21 @@ async def on_message(message):
             pfp = author.avatar_url
             embed.set_thumbnail(url=pfp)
             channel = client.get_channel(729923794881347585)
+            admin_role = get(guild.roles, name="Scripter")
+            await channel.send(admin_role.mention)
             sent = await channel.send(embed=embed)  # creates a message object called "sent"
             tickets.append(Ticket(sent, author, theMessage))
             await sent.add_reaction(emoji="\U0001F44D")  # use the message object to add the reaction to
             await sent.add_reaction(emoji="\U0001F44E")
+            channel2 = await author.create_dm()
+            embed = discord.Embed(title=f"{guild.name}", description=f"```{theMessage}```",
+                                  timestamp=datetime.datetime.utcnow(), color=discord.Color.red())
+            embed.add_field(name="Ticket User", value=f"{author}")
+            embed.add_field(name="Server ID", value=f"{guild.id}")
+            embed.set_footer(text="Request Has Been Created, Please Wait for a Staff Member To Accept!")
+            pfp = author.avatar_url
+            embed.set_thumbnail(url=pfp)
+            await channel2.send(embed=embed)
 
             var1 = discord.Embed(
                 footer='Request Accepted',
@@ -77,7 +97,9 @@ async def on_message(message):
                         # ticket claimed by user
                         ticket.state = TicketState.CLAIMED
                         admin_role = get(guild.roles, name="Scripter")
-                        channel = await guild.create_text_channel(str(display), overwrites=overwrites)
+                        category = discord.utils.get(guild.categories, id=731823656983855135)
+                        # , category=category
+                        channel = await guild.create_text_channel(str(display), overwrites=overwrites, category=category)
                         await channel.set_permissions(ticket.user, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
                         await channel.set_permissions(admin_role, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
                         new_embed = discord.Embed(title=f"{guild.name}", description=f"```{ticket.request}```",
@@ -97,6 +119,7 @@ async def on_message(message):
                         pfp = ticket.user.avatar_url
                         new_embed.set_thumbnail(url=pfp)
                         await ticket.message.edit(embed=new_embed)
+                        await channel.send(ticket.user.mention)
                         tickets.remove(ticket)
         else:
             await message.delete()
